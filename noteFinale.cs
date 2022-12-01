4bddd8150f902e9a8915714141bd29b58889745b
SoukaynaAbdellaoui <116548896+SoukaynaAbdellaoui@users.noreply.github.com>
2022-12-02 00:31:03 +0100

    Update noteFinale.cs

--- noteFinale.cs
+++ noteFinale.cs
@@ -1,50 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ExDiagrammeDeClasse
 {
     class noteFinale
     {
         private condidat candidate;
-        private Exam exam;
+        private exam ex;
         private List<reponse> reponse;
         private int ScoreTotal;
 
-        public noteFinale(condidat candidate, Exam exam)
+        public noteFinale(condidat candidate, exam ex)
         {
             this.candidate = candidate;
-            this.exam = exam;
+            this.ex = ex;
             this.reponse = new List<reponse>();
-            this.ScoreTotal = 0;
+
         }
         public void ajouterReponse(reponse res)
         {
             reponse.Add(res);
         }
 
-        public void scoreCalcul(Func<string, int> getQuestion)
-        {
-            foreach (reponse res in reponse)
-            {
-                if (res.question is questionOuverte)
-                {
-                    ScoreTotal += getQuestion(res.question.statement);
-                }
-                else
-                {
-                    ScoreTotal += res.question.ScoreInitiale;
 
-
-                }
-            }
-        }
     }
-
-    class responses
-    {
     }
-}
 
